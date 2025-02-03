797d0566d757d6c1e2e86278c443075ebd01e892
Dan Bar-Or <baror38@gmail.com>
2025-02-03 15:18:21 +0200

    commented out mongoDB cluster creation

--- modules/mongodb/variables.tf
+++ modules/mongodb/variables.tf
@@ -1,44 +1,44 @@
-variable "mongo_public_key" {
-  description = "Public key from atlas"
-  type = string
-}
-variable "mongo_private_key" {
-  description = "Private key from atlas"
-  type = string
-}
+# variable "mongo_public_key" {
+#   description = "Public key from atlas"
+#   type = string
+# }
+# variable "mongo_private_key" {
+#   description = "Private key from atlas"
+#   type = string
+# }
 
-variable "org_id" {
-  description = "MongoDB Atlas Organization ID"
-  type        = string
-}
+# variable "org_id" {
+#   description = "MongoDB Atlas Organization ID"
+#   type        = string
+# }
 
-variable "project_name" {
-  description = "MongoDB Atlas project name"
-  type        = string
-}
+# variable "project_name" {
+#   description = "MongoDB Atlas project name"
+#   type        = string
+# }
 
-variable "cluster_name" {
-  description = "MongoDB cluster name"
-  type        = string
-}
+# variable "cluster_name" {
+#   description = "MongoDB cluster name"
+#   type        = string
+# }
 
-variable "region" {
-  description = "MongoDB Atlas cluster region"
-  type        = string
-  default     = "US_EAST_1"
-}
+# variable "region" {
+#   description = "MongoDB Atlas cluster region"
+#   type        = string
+#   default     = "US_EAST_1"
+# }
 
-variable "db_user" {
-  description = "Database username"
-  type        = string
-}
+# variable "db_user" {
+#   description = "Database username"
+#   type        = string
+# }
 
-variable "db_password" {
-  description = "Database user password"
-  type        = string
-}
+# variable "db_password" {
+#   description = "Database user password"
+#   type        = string
+# }
 
-variable "database_name" {
-  description = "Database name for user permissions"
-  type        = string
-}
+# variable "database_name" {
+#   description = "Database name for user permissions"
+#   type        = string
+# }
